068dadba8d2517c9338e16e3549846165a17d145
Zack Walker <zwalker2016@gmail.com>
2020-11-04 19:17:46 -0600

    bootstrap form

--- wedding/weddingidea/couples/forms.py
+++ wedding/weddingidea/couples/forms.py
@@ -5,3 +5,10 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email']
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
--- wedding/weddingidea/couples/views.py
+++ wedding/weddingidea/couples/views.py
@@ -14,10 +14,10 @@ def landing_page(request):
 def registerPage(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        # profile_form = ProfileForm(request.POST, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
-            profile_form.save()
+            # profile_form.save()
             messages.success(request, _(
                 'Your profile was successfully updated!'))
             return redirect('settings:profile')

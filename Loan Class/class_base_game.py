3732b92d6c743650a1b3c8ce658de8c86cea7374
Zack Walker <zwalker2016@gmail.com>
2020-11-10 10:52:47 -0600

    test

--- Loan Class/class_base_game.py
+++
@@ -1,9 +0,0 @@
-class fighter:
-    def __init__(self,health, defense, attack):
-        self.health = health
-        self.defense = defense
-        self.attack = attack
-    
-    def remaining_health(self):
-        health - 
-
---
+++ wedding/weddingidea/accounts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from .forms import ProfileForm, CustomUserCreationForm
+
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = CustomUserCreationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save() 
+            return redirect('registration:registration')
+    else:
+        user_form = CustomUserCreationForm()
+        profile_form = ProfileForm(request.POST)
+        print(profile_form)
+    return render(request, 'registration/registration.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
